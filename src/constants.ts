47b5cb820cd4695e46f5d887e5129ea81dceff84
altov-shade <altovbrain2@gmail.com>
2026-02-11 12:49:31 -0500

    Create constants.ts

---
+++ src/constants.ts
@@ -0,0 +1,22 @@
+import { Shade } from "./types";
+
+export const SHADES: Shade[] = [
+  {
+    id: "HF5",
+    name: "HF5",
+    image: "/images/HF5.png",
+    undertone: "neutral",
+  },
+  {
+    id: "HF6",
+    name: "HF6",
+    image: "/images/HF6.png",
+    undertone: "warm",
+  },
+  {
+    id: "HF7",
+    name: "HF7",
+    image: "/images/HF7.png",
+    undertone: "cool",
+  },
+];
